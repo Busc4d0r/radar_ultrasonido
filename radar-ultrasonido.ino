c4f9693adfee4ce5565f40ed012bd0409a85dbf6
LRamos <112835204+Busc4d0r@users.noreply.github.com>
2023-12-04 00:18:16 -0400

    first commit

---
+++ radar-ultrasonido.ino
@@ -0,0 +1,52 @@
+#include <Servo.h>
+const int trigger = A1;
+const int echo = A0;
+const int indicador = LED_BUILTIN;
+long duracion;
+int distancia;
+Servo myServo;
+
+void setup() {
+  pinMode(trigger, OUTPUT);
+  pinMode(echo, INPUT);
+  pinMode(indicador, OUTPUT);
+  Serial.begin(9600);
+  myServo.attach(12);
+}
+
+void loop() {
+  for(int i=15;i<=165;i++){
+    myServo.write(i);
+    delay(30);
+    distancia = calcularDistancia();
+    if (distancia < 40) digitalWrite(indicador, HIGH);
+    else if(distancia > 40) digitalWrite(indicador, LOW);
+    Serial.print(i);
+    Serial.print(",");
+    Serial.print(distancia);
+    Serial.print(".");
+  }
+ 
+  for(int i=165;i>15;i--){
+    myServo.write(i);
+    delay(30);
+    distancia = calcularDistancia();
+    if (distancia < 40) digitalWrite(indicador, HIGH);
+    else if(distancia > 40) digitalWrite(indicador, LOW);
+    Serial.print(i);
+    Serial.print(",");
+    Serial.print(distancia);
+    Serial.print(".");
+  }
+}
+
+int calculateDistance(){
+  digitalWrite(trigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger, LOW);
+  duracion = pulseIn(echo, HIGH);
+  distancia = (duracion*0.0343)/2;
+  return distancia;
+}
